ed077c6440155142e478447953f288beff858513
Lyn <lynfinman@gmail.com>
2022-06-28 20:30:36 -0400

    commit tree 68aff8d9e58179308d1660596c947e47c64d4782

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word, :sorted_word
+
+  def initialize(word)
+    @word = word
+    @sorted_word = word.chars.sort.join
+  end
+
+  def match(words)
+    found_words = []
+    words.each do |word|
+      word.chars.sort.join == @sorted_word ? found_words.push(word) : word
+    end
+    found_words
+  end
+end
